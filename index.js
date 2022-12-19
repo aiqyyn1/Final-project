c0ba53b8972c67143ad5026ab205f9c50bc8d89b
Aikyn <ibrayevaiqyn@gmail.com>
2022-12-19 15:48:52 +0600

    kox

---
+++ index.js
@@ -0,0 +1,7 @@
+function hover(element) {
+  element.setAttribute('src', 'https://cdn.shopify.com/s/files/1/2028/6907/products/PLUSHIEE_1512x.jpg?v=1660755154');
+}
+
+function unhover(element) {
+  element.setAttribute('src', '	https://cdn.shopify.com/s/files/1/2028/6907/products/PLUSHIE_540x.jpg?v=1660755154');
+}
